191a23e9027132d39fcf6cf65716d70d2a73c7f1
hookaiogram <valerick1804@mail.ru>
2025-09-16 20:17:11 +0300

    Update aiogram_run.py

--- aiogram_run.py
+++ aiogram_run.py
@@ -1,24 +1,16 @@
-import os
-import asyncio
+import logging
 from aiogram.types import BotCommand, BotCommandScopeDefault
 from aiohttp import web
 from middlewares.db import DataBaseSession
 from database.engine import create_db, drop_db, session_maker
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from create_bot import bot, dp, BASE_URL, WEBHOOK_PATH, HOST, PORT, ADMIN_ID
 from handlers.admin_private import admin_router
 from handlers.user_group import user_group_router
-from aiogram import Bot, Dispatcher
+from handlers.user_private import user_private_router
 
-ADMIN_ID = "2031703859"
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEB_SERVER_HOST = "0.0.0.0"
-WEB_SERVER_PORT = int(os.getenv("PORT", 10000))
-WEBHOOK_PATH = "/webhook"
-BASE_WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 # Функция для установки командного меню для бота
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-bot.my_admin_list = [2031703859] 
+
 
 async def set_commands():
     # Создаем список команд, которые будут доступны пользователям
@@ -29,13 +21,13 @@ async def set_commands():
 
 
 async def on_startup() -> None:
-    await drop_db()
+    #await drop_db()
 
     await create_db()
 # Устанавливаем командное меню
     await set_commands()
 # Устанавливаем вебхук для приема сообщений через заданный URL
-    await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}")
+    await bot.set_webhook(f"{BASE_URL}{WEBHOOK_PATH}")
 # Отправляем сообщение администратору о том, что бот был запущен
     await bot.send_message(chat_id=ADMIN_ID, text='Бот запущен!')
 # Функция, которая будет вызвана при остановке бота
@@ -55,6 +47,7 @@ def main() -> None:
     # Подключаем маршрутизатор (роутер) для обработки сообщений
     dp.include_router(admin_router)
     dp.include_router(user_group_router)
+    dp.include_router(user_private_router)
 # Регистрируем функцию, которая будет вызвана при старте бота
     dp.startup.register(on_startup)
     dp.update.middleware(DataBaseSession(session_pool=session_maker))
@@ -70,17 +63,18 @@ def main() -> None:
 # Настраиваем приложение и связываем его с диспетчером и ботом
     setup_application(app, dp, bot=bot)
 # Запускаем веб-сервер на указанном хосте и порте
+    web.run_app(app, host=HOST, port=PORT)
 
-    return app
-# Запускаем веб-сервер на указанном хосте и порте
-    
-async def set_webhook():
-    await bot.set_webhook("https://hookaiogram-github-io.onrender.com")
 
 # Точка входа в программу
 if __name__ == "__main__":
-    app = (main())
-    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+    # Настраиваем логирование (информация, предупреждения, ошибки) и выводим их в консоль
+    logging.basicConfig(
+        level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(__name__)
+# Создаем логгер для использования в других частях программы
+    main()  # Запускаем основную функцию
+
 
 
 
